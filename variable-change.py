ab9f2c9c974cb3559501d34f30d445d6eb33ee72
Kutluay Aydın <100884457+Kutluay684@users.noreply.github.com>
2025-08-31 22:51:17 +0300

    Python Day5

---
+++ variable-change.py
@@ -0,0 +1,14 @@
+
+#number2=input("Enter number")
+#print(number1)
+#print(number2)
+#point=int(number1)+int(number2)
+#print(point)
+
+number1=int(input("Enter number"))
+number2=int(input("Enter number"))
+print(number1)
+print(number2)
+point=number1+number2
+print(point)
+
