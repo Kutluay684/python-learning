4823d540cc8e6ef6fa9fb52ca852f47a38f08f10
Kutluay Aydın <100884457+Kutluay684@users.noreply.github.com>
2025-09-02 18:30:47 +0300

    Python Day7

---
+++ lists-first-prt.py
@@ -0,0 +1,14 @@
+school="Izmır University Of Economics".split()
+print(school)
+print(school[0])
+print(school[1])
+
+numbers=[1,2,3,4,5,6]
+print(numbers)
+names=["MOUHAMMEDMUSAVİ","ALİMEHALİSAFİR","SALİHALLA"]
+print(names)
+print(type(names))
+print(names[0])
+studentnotes=["MOUHAMMMMEDDDMUSAVİLAIBRAHM",60,40,20]
+studenavaragenotes=(studentnotes[1]+studentnotes[2]+studentnotes[3])/3
+print(studenavaragenotes)
