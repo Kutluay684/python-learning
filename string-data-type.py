25e2ee3b1f0da12df3a6e0dc7647d1ddc12cfef8
Kutluay Aydın <100884457+Kutluay684@users.noreply.github.com>
2025-09-03 17:31:33 +0300

    Python Day7

---
+++ string-data-type.py
@@ -0,0 +1,12 @@
+
+
+placeName="Yakamoz Restaurant"
+placeDescription="Nice Place"
+placeTiming="20 Hours"
+
+message = "Place Name" + placeName + "Place Description" + placeDescription + "Time" + placeTiming
+
+print(message[-1])
+
+print(message[0])
+
