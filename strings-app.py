2caf412bba0d20d22c174b38e44f71ed0eeb924a
Kutluay Aydın <100884457+Kutluay684@users.noreply.github.com>
2025-09-06 16:20:30 +0300

    Python Day11

---
+++ strings-app.py
@@ -0,0 +1,20 @@
+
+title="Python better than Java"
+
+total=len(title)
+print(total)
+
+print(title[:6])
+print(title[::-1])
+print(title[:-6])
+print(title[::-6])
+print(title[0::])
+print(title[-1::])
+
+name=input("Enter Name")
+not1=(input("Enter not1"))
+not2=(input("Enter not2"))
+totalnote=(float(not1)+float(not2))/2
+final= f"Student name is {name} and average note is {totalnote}"
+
+print(final)
