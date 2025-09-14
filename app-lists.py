53e42293a6477ac97a18ee206607269b505afe5a
Kutluay Aydın <100884457+Kutluay684@users.noreply.github.com>
2025-09-14 20:03:45 +0300

    Python Day15

---
+++ app-lists.py
@@ -0,0 +1,14 @@
+brands="Mercedes","Iphone","Tesla"
+final=brands
+print(final)
+final="BMW" in brands
+print(final)
+final="BMW"not in brands
+
+print(final)
+
+student1=["Mouhammed Musavi",2025,[70,80,90]]
+student2=["Kutluay Aydın",2023,[50,40,30]]
+
+students=[student1,student2]
+print(students)
